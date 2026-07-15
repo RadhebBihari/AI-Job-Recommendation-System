6543e5e202016cbc75447cb5b7f43cb6fd66f8dc
Radhe Bihari <vipulkumar1227@gmail.com>
2026-07-15 18:56:12 +0530

    MongoDB Atlas connected successfully and JWT authentication completed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,10 +2,10 @@ const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
   try {
+    // Get Authorization Header
     const authHeader = req.headers.authorization;
 
-    console.log("Authorization Header:", authHeader);
-
+    // Check if token exists
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({
         success: false,
@@ -13,21 +13,17 @@ const protect = (req, res, next) => {
       });
     }
 
+    // Extract Token
     const token = authHeader.split(" ")[1];
 
-    console.log("Token:", token);
-    console.log("JWT Secret:", process.env.JWT_SECRET);
-
+    // Verify Token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log("Decoded:", decoded);
-
+    // Store user information in request
     req.user = decoded;
 
     next();
   } catch (error) {
-    console.log("JWT VERIFY ERROR:", error.message);
-
     return res.status(401).json({
       success: false,
       message: "Invalid or Expired Token",
